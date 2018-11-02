213a3ceaf95b3b8a1c9ca25eeef2b1ead94b6459
Queen Amina <admin@azhiya.com>
2018-11-02 21:35:14 +0000

    custom post types, advanced custom fields, add to home page, created custom archive, imported different platform and font awesome menu

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -17,10 +17,35 @@ get_header(); ?>
 		<div class="main-content" role="main">
 			<?php while ( have_posts() ) : the_post(); ?>
 				<?php the_content(); ?>
-				<a class="button" href="<?php echo site_url('/blog/') ?>">View Our Work</a>
+				<a class="button" href="<?php echo site_url('/case-studies/') ?>">View Our Work</a>
 			<?php endwhile; // end of the loop. ?>
 		</div><!-- .main-content -->
 	</div><!-- #primary -->
+
+	<section class="featured-wordk">
+		<div class="site-content">
+			<div class="featured-work-post">
+				<h4>Featured Work</h4>
+
+				<ul class = "homepage-featured-work">
+					<?php query_posts('posts_per_page=3&post_type=case_studies&order=ASC'); ?>
+						<?php while (have_posts() ) : the_post(); 
+							$image_1 = get_field("image_1");
+							$size = "medium";
+						?>
+							<li class="individual-featured-work">
+								<figure><a href="<?php echo get_post_permalink(); ?>">
+									<?php echo wp_get_attachment_image($image_1, $size); ?></a>
+								</figure>
+
+								<h3><a href="<?php echo get_post_permalink(); ?>"><?php the_title(); ?></a></h3>
+							</li>
+						<?php endwhile; ?>
+					<?php wp_reset_query(); ?>
+				</ul>
+			</div>
+		</div>
+	</section>
 
 	<section class="recent-posts">
 		<div class="site-content">
--- wp-content/themes/accelerate-theme-child/functions.php
+++ wp-content/themes/accelerate-theme-child/functions.php
@@ -16,3 +16,38 @@ function accelerate_child_scripts(){
 	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'accelerate-style' ));
 }
 add_action( 'wp_enqueue_scripts', 'accelerate_child_scripts' );
+
+//Custom post types
+function create_custom_post_types() {
+    register_post_type( 'case_studies',
+        array(
+            'labels' => array(
+                'name' => __( 'Case Studies' ),
+                'singular_name' => __( 'Case Study' )
+            ),
+            'public' => true,
+            'has_archive' => true,
+            'rewrite' => array( 'slug' => 'case-studies' ),
+        )
+    );
+}
+add_action( 'init', 'create_custom_post_types' );
+
+
+//Add Font Awesome Icons
+function enqueue_load_fa() {
+    wp_enqueue_style( 'load-fa', 'https://use.fontawesome.com/releases/v5.5.0/css.all.css');
+}
+add_action( 'wp_enqueue_scripts', 'enqueue_load_fa' );
+
+
+
+// Reverse Case Studies Archive order
+//function reverse_archive_order( $query ){
+//
+//    if( !is_admin() && $query->is_post_type_archive('case_studies')  && $query->is_main_query() ) {
+//        $query->set('order', 'ASC');
+//    }
+//}
+//
+//add_action( 'pre_get_posts', 'reverse_archive_order' );
