f4ac01078694629c2032f573fc485e2038438841
Queen Amina <admin@azhiya.com>
2019-02-19 20:16:50 +0000

    styled the about page

--- wp-content/themes/accelerate-theme-child/page-about.php
+++ wp-content/themes/accelerate-theme-child/page-about.php
@@ -29,7 +29,7 @@ get_header(); ?>
 			<p>We take pride in our clients and the content we create for them.<br>Here's a brief overview of our offered services.</p>	
 			<div class="services-wrapper">
 				<ul>
-				<?php query_posts('post_type=services'); ?>
+				<?php query_posts('post_type=services&order=ASC'); ?>
 					<?php while (have_posts() ) : the_post(); 
 						$service_icon = get_field("service_icon");
 						$size ="small";
@@ -39,7 +39,7 @@ get_header(); ?>
 							<figure><?php echo wp_get_attachment_image($service_icon, $size); ?>
 							</figure>
 						</div>
-						<div>
+						<div class="service-detail">
 							<h2><?php the_title(); ?></h2>
 							<p><?php the_content(); ?></p>
 						</div>
@@ -50,7 +50,7 @@ get_header(); ?>
 			</div>
 
 			<div class="about-contact">
-					<div>
+					<div class="about-contact-h2"> 
 						<h2>Interested in working with us?</h2>
 					</div>
 					<div>
