0c1a26c3bd2ac5b4be75e787d701bd2286edaef1
Queen Amina <admin@azhiya.com>
2019-02-20 17:45:06 +0000

    created a 404 page and styles, enqueued google font to child theme

---
+++ wp-content/themes/accelerate-theme-child/404.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * The 404 page
+ *
+ * This is the 404 error page.
+ *
+ * @package WordPress
+ * @subpackage Accelerate Marketing
+ * @since Accelerate Marketing 2.0
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="site-content sidebar">
+		<div class="main-content" role="main">
+		</div><!-- .main-content -->
+
+	</div><!-- #primary -->
+
+    <section>
+    <div class="site-content">
+        <div class="funny-box">
+            <div style="width:100%;height:0;padding-bottom:75%;position:relative;">
+                <iframe src="https://giphy.com/embed/kQbMO5X7UA1C8" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
+            </div>
+            <p><a href="https://giphy.com/gifs/kQbMO5X7UA1C8">via GIPHY</a></p>
+        </div>
+        
+        <div class="serious-box">
+            <h1>Oops! We can't find that page....</h1>
+            <p>Sorry for that. Feel free to take a look around our <a href="<?php echo site_url('/blog/') ?>">blog</a> or some of our <a href="<?php echo site_url('/case-studies/') ?>">featured work</a>.</p>
+        </div>
+    </section>
+
+<?php get_footer(); ?>
