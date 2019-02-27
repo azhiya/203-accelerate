66e83d5973006bebf0acf5c1fae2da308ed6c05e
Queen Amina <admin@azhiya.com>
2019-02-27 19:42:47 +0000

    mailchip and customer twitter feed plugin added

--- wp-content/themes/accelerate-theme-child/404.php
+++ wp-content/themes/accelerate-theme-child/404.php
@@ -19,14 +19,14 @@ get_header(); ?>
 
     <section>
     <div class="site-content">
-        <div class="funny-box">
+        <div id="funny404-box">
             <div style="width:100%;height:0;padding-bottom:75%;position:relative;">
                 <iframe src="https://giphy.com/embed/kQbMO5X7UA1C8" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
             </div>
             <p><a href="https://giphy.com/gifs/kQbMO5X7UA1C8" target="_BLANK">via GIPHY</a></p>
         </div>
         
-        <div class="serious-box">
+        <div id="serious404-box">
             <h1>Oops! We can't find that page....</h1>
             <p>Sorry for that. Feel free to take a look around our <a href="<?php echo site_url('/blog/') ?>">blog</a> or some of our <a href="<?php echo site_url('/case-studies/') ?>">featured work</a>.</p>
         </div>
--- wp-content/themes/accelerate-theme-child/functions.php
+++ wp-content/themes/accelerate-theme-child/functions.php
@@ -54,6 +54,21 @@ function enqueue_load_fa() {
 add_action( 'wp_enqueue_scripts', 'enqueue_load_fa' );
 
 
+//Adding a new dynamic sidebar
+function accelerate_theme_child_widget_init() {
+	
+	register_sidebar( array(
+	    'name' =>__( 'Homepage sidebar', 'accelerate-theme-child'),
+	    'id' => 'sidebar-2',
+	    'description' => __( 'Appears on the static front page template', 'accelerate-theme-child' ),
+	    'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+	    'after_widget' => '</aside>',
+	    'before_title' => '<h3 class="widget-title">',
+	    'after_title' => '</h3>',
+	) );
+	
+}
+add_action( 'widgets_init', 'accelerate_theme_child_widget_init' );
 
 
 // Reverse Case Studies Archive order
