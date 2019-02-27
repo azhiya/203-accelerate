f3780a689376dfee9b2916dbb35f3555b991de3a
Queen Amina <admin@azhiya.com>
2019-02-27 13:44:02 +0000

    updated translations, 2019 theme and WP 5.1

--- wp-content/themes/accelerate-theme-child/404.php
+++ wp-content/themes/accelerate-theme-child/404.php
@@ -23,7 +23,7 @@ get_header(); ?>
             <div style="width:100%;height:0;padding-bottom:75%;position:relative;">
                 <iframe src="https://giphy.com/embed/kQbMO5X7UA1C8" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
             </div>
-            <p><a href="https://giphy.com/gifs/kQbMO5X7UA1C8">via GIPHY</a></p>
+            <p><a href="https://giphy.com/gifs/kQbMO5X7UA1C8" target="_BLANK">via GIPHY</a></p>
         </div>
         
         <div class="serious-box">
